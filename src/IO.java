27e6bc35b56e6f7093b5c6229b3bd43a2f5e1d0c
HobertJ <13521079@std.ste.itb.ac.id>
2022-09-30 20:27:10 +0700

    add io

---
+++ src/IO.java
@@ -0,0 +1,11 @@
+import java.io.File;  
+import java.io.FileNotFoundException;  
+import java.util.Scanner;
+import java.io.FileWriter;   
+import java.io.IOException;
+
+public class IO {
+    public static Matrix append(Matrix list1, Matrix list2){
+        Matrix
+    }
+}
